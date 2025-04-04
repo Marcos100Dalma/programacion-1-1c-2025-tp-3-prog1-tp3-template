0ef11a0581a04a8edca28bd661c634ef7d16c03f
Marcos100Dalma <mdalma@mail.austral.edu.ar>
2025-04-04 12:32:56 -0300

    Update tp3_slice_simple_test.py

--- tp3_slice_simple_test.py
+++ tp3_slice_simple_test.py
@@ -1,18 +1,5 @@
-import io
-import unittest.mock
-import slice_simple as ex2
-
-class TP3SliceSimpleTestCases(unittest.TestCase):
-
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_slice_simple(self, mock_stdout):
-        ex2.slice_simple()
-        results = mock_stdout.getvalue().splitlines()
-
-        # Matias should contain A and I
-        self.assertEqual(results[0], "awe")
-        self.assertEqual(results[1], "eso")
-        self.assertEqual(results[2], "awesome")
-
-if __name__ == '__main__':
-    unittest.main()
+def slice_simple():
+    texto = "Awesome"
+    print(texto[:3].lower()) # awe
+    print(texto[2:5]) # eso
+    print(texto.lower()) # awesome
